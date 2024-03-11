c44e6303b74d82c667ffa70e01b7ae16bee8eab7
Samir Dangal <samirdangal@gmail.com>
2024-03-11 14:52:46 +0545

    fixes PMD check failure

--- collect_app/src/main/java/org/odk/collect/android/geo/MapLayerSourceProvider.kt
+++ collect_app/src/main/java/org/odk/collect/android/geo/MapLayerSourceProvider.kt
@@ -1,12 +1,8 @@
 package org.odk.collect.android.geo
 
-import org.odk.collect.android.openrosa.OpenRosaFormSource
 import org.odk.collect.android.openrosa.OpenRosaHttpInterface
-import org.odk.collect.android.openrosa.OpenRosaResponseParserImpl
 import org.odk.collect.android.utilities.WebCredentialsUtils
-import org.odk.collect.forms.FormSource
 import org.odk.collect.settings.SettingsProvider
-import org.odk.collect.settings.keys.ProjectKeys
 
 class MapLayerSourceProvider(
     private val settingsProvider: SettingsProvider,
@@ -14,13 +10,13 @@ class MapLayerSourceProvider(
 ) {
 
     @JvmOverloads
-    fun get(serverURL: String? = null,projectId: String? = null): MapLayerSource {
+    fun get(serverURL: String? = null, projectId: String? = null): MapLayerSource {
         val generalSettings = settingsProvider.getUnprotectedSettings(projectId)
 
         return MbTilesSource(
             serverURL,
             mbTileHttpInterface,
-            WebCredentialsUtils(generalSettings),
+            WebCredentialsUtils(generalSettings)
         )
     }
 }
