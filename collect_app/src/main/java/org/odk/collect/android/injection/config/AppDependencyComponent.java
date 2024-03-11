951277b40c7e96287529bd059eb39c725bc9b75e
Samir Dangal <samirdangal@gmail.com>
2024-03-11 14:34:28 +0545

    fixes PMD checks fail issue

--- collect_app/src/main/java/org/odk/collect/android/injection/config/AppDependencyComponent.java
+++ collect_app/src/main/java/org/odk/collect/android/injection/config/AppDependencyComponent.java
@@ -49,8 +49,6 @@ import org.odk.collect.android.fragments.dialogs.SelectMinimalDialog;
 import org.odk.collect.android.gdrive.GoogleDriveActivity;
 import org.odk.collect.android.gdrive.GoogleSheetsUploaderActivity;
 import org.odk.collect.android.geo.GoogleMapFragment;
-import org.odk.collect.android.geo.MapLayerSourceProvider;
-import org.odk.collect.android.geo.MbTilesHttpInterface;
 import org.odk.collect.android.mainmenu.MainMenuActivity;
 import org.odk.collect.android.openrosa.OpenRosaHttpInterface;
 import org.odk.collect.android.preferences.CaptionedListPreference;
